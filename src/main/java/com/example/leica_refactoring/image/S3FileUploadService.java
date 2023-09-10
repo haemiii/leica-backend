5ab15378603768df51f9b87a7e3290364a0d4bc2
seonghun <sdzx0719@naver.com>
2023-09-10 21:06:03 +0900

    added s3

---
+++ src/main/java/com/example/leica_refactoring/image/S3FileUploadService.java
@@ -0,0 +1,47 @@
+package com.example.leica_refactoring.image;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Service
+public class S3FileUploadService {
+
+    private final AmazonS3 s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.s3.default-url}")
+    private String defaultUrl;
+
+    public String uploadFile(MultipartFile file) throws IOException{
+        String fileName = generateFileName(file);
+        String imageUrl = defaultUrl + fileName;
+        try {
+            s3Client.putObject(bucketName, fileName, file.getInputStream(), getObjectMetadata(file));
+            return imageUrl;
+        } catch (SdkClientException e) {
+            throw new IOException("S3에 파일 업로드를 실패하였습니다.",e);
+        }
+    }
+
+
+    private ObjectMetadata getObjectMetadata(MultipartFile file){
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(file.getContentType());
+        objectMetadata.setContentLength(file.getSize());
+        return objectMetadata;
+    }
+
+    private String generateFileName(MultipartFile file){
+        return file.getOriginalFilename();
+    }
+}
